af66d252b78b05a5cc5e5d5b7ced239cc8af0c9e
xHenrySx <enrisal2000@gmail.com>
2024-04-07 16:36:41 -0400

    Actualizar componente MenuItem para agregar funcionalidad de agregar ítem al pedido

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,18 @@
-const MenuItem = () => {
+import type { MenuItem as MenuItemType } from "../types";
+type MenuItemProps = {
+  item: MenuItemType,
+  addItem: (item: MenuItemType) => void
+};
+const MenuItem = ({ item, addItem }: MenuItemProps) => {
   return (
-    <div></div>
-  )
-}
+    <button 
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      onClick={() => addItem(item)}
+    >
+      <p className="font-black">{item.name}</p>
+      <p>${item.price}</p>
+    </button>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
