d8654aa1c04a9386b66ae1d0874bd15a7bc0d40e
xHenrySx <enrisal2000@gmail.com>
2024-04-07 15:58:57 -0400

    Añadir estilos al encabezado y secciones en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { menuItems } from "./data/db";
 
 function App() {
-  
   return (
     <>
-      <header className=""></header>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+
+        <div>
+          <h2>Menu</h2>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
