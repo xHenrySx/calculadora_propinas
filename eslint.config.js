237592ce21c35710a33f944ad3a6646162fd5d75
xHenrySx <enrisal2000@gmail.com>
2024-07-26 22:39:43 -0400

    Cambios en la configuracion del linter

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import path from 'path';
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
   pluginReact.configs.flat.recommended,
   {
     rules: {
-      // Add your custom rules here if needed
+      //Add
+      // "@typescript-eslint/explicit-module-boundary-types": "error",
     },
     settings: {
       react: {
@@ -21,7 +22,7 @@ export default [
     },
   },
   {
-    files: ["**/*.{jsx,tsx}"],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
       "react/react-in-jsx-scope": "off",
     },
