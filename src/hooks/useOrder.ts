50858c40f0945b5fcc4714bae2f81b1d614ec707
xHenrySx <enrisal2000@gmail.com>
2024-07-26 22:39:58 -0400

    Agrega customHook

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,13 +3,35 @@ import type { MenuItem, OrderItem } from "../types";
 
 const useOrder = () => {
   const [order, setOrder] = useState<OrderItem[]>([]);
+  const [tip, setTip] = useState(0);
 
   const addItem = (item: MenuItem) => {
-    const newItem = { ...item, quantity: 1 };
-    setOrder([...order, newItem]);
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+
+    if (!itemExist) {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+      return;
+    }
+
+    const updatedOrder = order.map((orderItem) =>
+      orderItem.id === item.id
+        ? { ...orderItem, quantity: orderItem.quantity + 1 }
+        : orderItem
+    );
+    setOrder(updatedOrder);
   };
 
-  return { addItem };
+  const removeItem = (id: MenuItem["id"]) => {
+    setOrder(order.filter((orderItem) => orderItem.id !== id));
+  };
+
+  const placeOrder = () => {
+    setOrder([])
+    setTip(0)
+  };
+
+  return { order, tip, setTip, addItem, removeItem, placeOrder };
 };
 
 export default useOrder;
